b559dfa3098a6ebee871043f2bcbfa29c09a70c6
Facundo Sandoval <fatuh1.7@hotmail.com>
2023-01-09 11:29:15 -0300

    Agregado modules y routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListTrabajosComponent } from './list-trabajos/list-trabajos.component';
+import { TrabajosComponent } from './trabajos/trabajos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'list-trabajos', component: ListTrabajosComponent },
+  { path: 'trabajos', component: TrabajosComponent  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListTrabajosComponent } from './list-trabajos/list-trabajos.component';
+import { TrabajosComponent } from './trabajos/trabajos.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgImageFullscreenViewModule } from 'ng-image-fullscreen-view';
+import { RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListTrabajosComponent,
+    TrabajosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    ReactiveFormsModule,
+    NgImageFullscreenViewModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
